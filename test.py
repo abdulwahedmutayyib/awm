570c5e7bb48c87b8bad8336f8a54c82c266dda2d
abdulwahedmutayyib <109347636+abdulwahedmutayyib@users.noreply.github.com>
2023-08-22 23:15:29 +0530

    Create test.py

---
+++ test.py
@@ -0,0 +1,19 @@
+import unittest
+import calculator  # Assuming you've saved your calculator code in a file named "calculator.py"
+
+class TestCalculatorFunctions(unittest.TestCase):
+
+    def test_add(self):
+        self.assertEqual(calculator.add(2, 3), 5)
+        self.assertEqual(calculator.add(-1, 1), 0)
+        self.assertEqual(calculator.add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(calculator.subtract(5, 3), 2)
+        self.assertEqual(calculator.subtract(1, 1), 0)
+        self.assertEqual(calculator.subtract(0, 0), 0)
+
+    # Similarly, write test methods for other functions...
+
+if __name__ == '__main__':
+    unittest.main()
