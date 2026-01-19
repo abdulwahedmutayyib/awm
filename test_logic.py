cfb6a710da742af787c03334ba5e424f7a2b5dcd
wahed <wahedmutayyib@gmail.com>
2026-01-19 16:04:33 +0530

    made changes to test_logic.py

--- test_logic.py
+++ test_logic.py
@@ -1,25 +1,33 @@
 from logic import calculate
 
+
 def test_add():
     assert calculate("10+20") == 30
+
 
 def test_subtract():
     assert calculate("20-5") == 15
 
+
 def test_multiply():
     assert calculate("5*6") == 30
+
 
 def test_divide():
     assert calculate("10/2") == 5
 
+
 def test_sqrt():
     assert calculate("sqrt(16)") == 4
+
 
 def test_power():
     assert calculate("2x^y3") == 8
 
+
 def test_sin():
     assert round(calculate("sin(0)"), 2) == 0
+
 
 def test_error():
     assert calculate("10/0") == "Error"
